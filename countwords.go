befe614a426abf106b9d7258d4317c4c6715592a
tgbaa <elvistaver@gmail.com>
2026-05-13 11:21:44 +0100

    word count

---
+++ countwords.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func CountWords(text string) map[string]int {
+
+	words := strings.Fields(text)
+	count:=make(map[string]int)
+
+	for _, word:= range words{
+		word=strings.ToLower(word)
+		word= strings.TrimFunc(word, func (r rune)bool{
+			return unicode.IsPunct(r)
+		})
+		count[word]++
+	}
+	return count
+}
+
+func main(){
+
+	text:="go is good to learn and go is fast...."
+
+	result:=CountWords(text)
+
+	for word, count:= range result{
+
+		fmt.Printf("%s:%d\n",word, count)
+	}
+	//fmt.Println(result)
+}
